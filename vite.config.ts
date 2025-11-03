7fa39f50e37e810517055655df5b278dc472880b
redcloud442 <markivor.glorioso@gmail.com>
2025-11-04 00:11:01 +0800

    add toml

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import tailwindcss from "@tailwindcss/vite";
-import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
+
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
-
 const config = defineConfig({
   plugins: [
-    nitroV2Plugin(),
     // this is the plugin that enables path aliases
+    netlify(),
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
